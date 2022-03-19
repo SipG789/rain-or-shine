91cf70cc7641a8df58332d1cbb40f659b3c6cacc
Sip <sipg789@gmail.com>
2022-03-19 12:07:52 -0500

    added todo statements to script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchCities;
 var searchBtn = document.querySelector(".btn");
 var cityInput = document.querySelector("#city");
 
-
+// TODO: display current weather data of a city on to page 
 
 var getWeather = function () {
     fetch (
@@ -15,7 +15,15 @@ var getWeather = function () {
 var anyCitySearch = function () {
     searchCities = cityInput.value;
     getWeather(searchCities);
+    console.log(anyCitySearch);
 };
+// TODO: UV index and have it display color code use if then statement 
+
+// TODO: fetch api for 5 day forecast  
+
+// TODO: convert data time 
+
+// TODO: add search history to local storage-- build search history with moment/js
 
 
 searchBtn.addEventListener("click", anyCitySearch);
