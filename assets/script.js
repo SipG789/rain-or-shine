9cc51427dd6121074cbfd7bbce3719dbbcaae9bd
Sip <sipg789@gmail.com>
2022-03-19 14:59:35 -0500

    stopping point

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,37 @@ var searchBtn = document.querySelector(".btn");
 var cityInput = document.querySelector("#city");
 
 // TODO: display current weather data of a city on to page 
-
 var getWeather = function () {
-    fetch (
-        "http://api.openweathermap.org/geo/1.0/direct?q=" + searchCities + "&appid=" + apiKey) 
-    
-        console.log (getWeather);
-    }
-
+// fetch api request by city/location name 
+    fetch(
+        "http://api.openweathermap.org/geo/1.0/direct?q=" + 
+        searchCities + 
+        "&appid=" + 
+        apiKey
+        ).then(function (data) {
+           console.log("this is working");
+            // return rsponse.json();
+        });
+        // .then(function (data) {
+        //     var latitude = data[0].lat;
+        //     var longitude = data[0].lon;
+        //     return [latitude, longitude];
+        // })
+        // .then(function (value) {    
+        //     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + value[0]+ "&lon=" + value[1] + "&appid=" + apiKey)
+        //     .then(function(response) {
+        //         console.log(response);
+        //         return response.json();
+        //     })
+        //     .then(function (data) {
+        //         console.log(data);
+        //     });
+        // });
+};
+// search the city 
 var anyCitySearch = function () {
     searchCities = cityInput.value;
     getWeather(searchCities);
-    console.log(anyCitySearch);
 };
 // TODO: UV index and have it display color code use if then statement 
 
